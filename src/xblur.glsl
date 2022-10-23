4309528daeb9b7cd14ac65fe458bdde3bc0259c5
thajohns <thajohns@clarkson.edu>
2022-10-23 16:50:01 -0400

    aha, sampler coordinates are weird

--- src/xblur.glsl
+++ src/xblur.glsl
@@ -10,12 +10,14 @@ layout(set = 0, binding = 1, rgba8) uniform writeonly image2D xblur;
 
 void main() {
   vec4 blur_output = vec4(0.0, 0.0, 0.0, 0.0);
-  vec2 coords = vec2(gl_GlobalInvocationID.xy);
+  ivec2 imgsz = imageSize(xblur);
+  float pixel_offset = 1.0 / float(imgsz.x);
+  vec2 coords = (vec2(gl_GlobalInvocationID.xy) + vec2(0.5, 0.5)) / imgsz;
   for (int i = 0; i < num_steps; i++) {
     blur_output += weights[i] *
       (
-       texture(arena, coords + vec2(offsets[i], 0.0)) +
-       texture(arena, coords - vec2(offsets[i], 0.0))
+       texture(arena, coords + vec2(pixel_offset * offsets[i], 0.0)) +
+       texture(arena, coords - vec2(pixel_offset * offsets[i], 0.0))
       );
   }
   imageStore(xblur, ivec2(gl_GlobalInvocationID.xy), blur_output);
